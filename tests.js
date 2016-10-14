ff3ce3c71b412e521c0a9d444eaaa3d9f78e882d
faksam <fakunlesamuel@gmail.com>
2016-10-14 18:03:26 +0100

    New Test
    Added First and second test

--- tests.js
+++ tests.js
@@ -15,4 +15,13 @@ describe("Test that constants are computed properly", function() {
   });
 });
 
+//2
+
+it("Incorrect!!!, Sum of primes between 0 - 10 is 17", function() {
+  assert(
+    lib.compareCoefficients(lib.sumOfPrimes(10)==17)
+  );
+});
+});
+
 });
